2044f52950cb3e2ffc64fda63d812f223e6d89f2
Dimitri Glazkov <dimitri@glazkov.com>
2023-05-13 21:55:48 -0700

    Add more tests.

--- core/library/tests/chat.ts
+++ core/library/tests/chat.ts
@@ -16,3 +16,63 @@ test("Constructor argument pass-through works as advertised", (t) => {
     t.is(chat.temperature, undefined);
   }
 });
+
+test("addMessage() works as advertised", (t) => {
+  const chat = new Chat();
+  t.deepEqual(chat.prompt, { messages: [] });
+
+  chat.addMessage("Hello there!");
+  t.deepEqual(chat.prompt, { messages: [{ content: "Hello there!" }] });
+
+  chat.addMessage("General Kenobi!");
+  t.deepEqual(chat.prompt, {
+    messages: [{ content: "Hello there!" }, { content: "General Kenobi!" }],
+  });
+});
+
+test("addExample() works as advertised", (t) => {
+  const chat = new Chat();
+
+  chat.addExample({ input: "What is the meaning of life?", output: "42" });
+  t.deepEqual(chat.prompt, {
+    examples: [
+      {
+        input: { content: "What is the meaning of life?" },
+        output: { content: "42" },
+      },
+    ],
+    messages: [],
+  });
+  chat.addExample({
+    input: "Pull up! All craft pull up!",
+    output: "It's a trap!",
+  });
+  t.deepEqual(chat.prompt, {
+    examples: [
+      {
+        input: { content: "What is the meaning of life?" },
+        output: { content: "42" },
+      },
+      {
+        input: { content: "Pull up! All craft pull up!" },
+        output: { content: "It's a trap!" },
+      },
+    ],
+    messages: [],
+  });
+});
+
+test("context() works as advertised", (t) => {
+  const chat = new Chat();
+
+  chat.context("What is the meaning of life?");
+  t.deepEqual(chat.prompt, {
+    context: "What is the meaning of life?",
+    messages: [],
+  });
+  chat.context("Pull up! All craft pull up!");
+  t.deepEqual(chat.prompt, {
+    context: "Pull up! All craft pull up!",
+    messages: [],
+  });
+});
